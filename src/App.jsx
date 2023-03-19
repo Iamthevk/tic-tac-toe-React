84243ce8b5c972721b166105b13cd04c39fe8103
Vijay Kumar <vijay.vuex@gmail.com>
2023-03-19 19:51:16 +0530

    board created with onClick value X when clicking square

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
-import React from "react";
-import "./styles.css"
+import React, { useState } from "react";
+import "./styles.css";
+
+function Square() {
+  const [value, setValue] = useState("null");
+  const handleClick = () => {
+    // console.log("clicked")
+    setValue("X");
+  };
+  return (
+    <button onClick={handleClick} className="square">
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-  <p>Hello React</p>
+      <Board />
     </div>
-  )
+  );
 }
 
 export default App;

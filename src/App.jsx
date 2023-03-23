8466b573a9d4f8b1d4ac4834f1beb2f274ead27c
Vijay Kumar <vijay.vuex@gmail.com>
2023-03-23 19:04:49 +0530

    implemented logic for winner and rendered whose turn is next on UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./styles.css";
 function Square({ value, onSquareClick }) {
   return (
     <button onClick={onSquareClick} className="square">
-      {value}
+      <p style={{ fontSize: "40px" }}>{value}</p>
     </button>
   );
 }
@@ -15,29 +15,62 @@ function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const handleClick = (i) => {
     // here we are returning if square[i] has a value already, this will prevent user to click if square has a value.
-    if (squares[i]) return;
+    if (squares[i] || calculateWinner(squares)) return;
     //here we created a copy of array with slice method
     const nextSquares = squares.slice();
     xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
+    calculateWinner(squares);
   };
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   return (
     <div>
+       <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </div>
   );
